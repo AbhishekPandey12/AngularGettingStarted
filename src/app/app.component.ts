b5c99aec5bde5a5524f3f759d236879947c6a82d
abhishekkumarpandey <abhisonu3629@gmail.com>
2020-05-02 23:51:17 +0530

    First Component Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'pm-root',
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+    <div>My First Component</div>
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularGettingStarted';
+  pageTitle: string = 'Acme Product Management';
 }
